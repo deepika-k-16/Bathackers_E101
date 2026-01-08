d31c4a79b7f23915a187978225301f1a7c677001
Jin Mori VMD <dhana006vmd@gmail.com>
2026-01-08 09:45:20 +0530

    Add files via upload

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL not set. Check your .env file.");
+}
+
+export default defineConfig({
+  out: "./migrations",
+  schema: "./shared/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
+});
